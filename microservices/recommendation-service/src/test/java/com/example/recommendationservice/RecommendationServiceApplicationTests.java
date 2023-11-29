193c379f8c84c5919c8f4c82dc942eaba2323e70
Мирослав Косюк <87585769+Baldezxniy@users.noreply.github.com>
2023-11-29 20:20:52 +0200

    Recommendation  service: added spring cloud stream, for asynchronous interaction, also simple tests

--- microservices/recommendation-service/src/test/java/com/example/recommendationservice/RecommendationServiceApplicationTests.java
+++ microservices/recommendation-service/src/test/java/com/example/recommendationservice/RecommendationServiceApplicationTests.java
@@ -1,32 +1,43 @@
 package com.example.recommendationservice;
 
+import static com.example.api.event.Event.Type.CREATE;
+import static com.example.api.event.Event.Type.DELETE;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static reactor.core.publisher.Mono.just;
 
 import com.example.api.core.recommendation.Recommendation;
+import com.example.api.event.Event;
+import com.example.api.exceptions.InvalidInputException;
 import com.example.recommendationservice.persistence.RecommendationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.test.StepVerifier;
+
+import java.util.function.Consumer;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class RecommendationServiceApplicationTests extends PostgreSQLTestBase {
-
   @Autowired
   private WebTestClient client;
-
   @Autowired
   private RecommendationRepository repository;
 
+  @Qualifier("messageProcessor")
+  @Autowired
+  private Consumer<Event<Integer, Recommendation>> messageProcessor;
+
   @BeforeEach
   void setupDb() {
-    repository.deleteAll().block();
+    StepVerifier.create(repository.deleteAll()).verifyComplete();
   }
 
   @Test
@@ -34,11 +45,11 @@ class RecommendationServiceApplicationTests extends PostgreSQLTestBase {
 
     int productId = 1;
 
-    postAndVerifyRecommendation(productId, 1, OK);
-    postAndVerifyRecommendation(productId, 2, OK);
-    postAndVerifyRecommendation(productId, 3, OK);
+    sendCreateRecommendationEvent(productId, 1);
+    sendCreateRecommendationEvent(productId, 2);
+    sendCreateRecommendationEvent(productId, 3);
 
-    assertEquals(3, repository.findAllByProductId(productId).count().block());
+    assertEquals(3, (long) repository.findAllByProductId(productId).count().block());
 
     getAndVerifyRecommendationsByProductId(productId, OK)
             .jsonPath("$.length()").isEqualTo(3)
@@ -52,15 +63,15 @@ class RecommendationServiceApplicationTests extends PostgreSQLTestBase {
     int productId = 1;
     int recommendationId = 1;
 
-    postAndVerifyRecommendation(productId, recommendationId, OK)
-            .jsonPath("$.productId").isEqualTo(productId)
-            .jsonPath("$.recommendationId").isEqualTo(recommendationId);
+    sendCreateRecommendationEvent(productId, recommendationId);
 
     assertEquals(1, repository.count().block());
 
-    postAndVerifyRecommendation(productId, recommendationId, UNPROCESSABLE_ENTITY)
-            .jsonPath("$.path").isEqualTo("/v1/recommendation")
-            .jsonPath("$.message").isEqualTo("Duplicate key, Product Id: 1, Recommendation Id:1");
+    InvalidInputException thrown = assertThrows(InvalidInputException.class,
+            () -> sendCreateRecommendationEvent(productId, recommendationId),
+            "Expect InvalidInputException hare!"
+    );
+    assertEquals("Duplicate key, Product Id: 1, Recommendation Id:1", thrown.getMessage());
 
     assertEquals(1, repository.count().block());
   }
@@ -71,13 +82,13 @@ class RecommendationServiceApplicationTests extends PostgreSQLTestBase {
     int productId = 1;
     int recommendationId = 1;
 
-    postAndVerifyRecommendation(productId, recommendationId, OK);
-    assertEquals(1, repository.findAllByProductId(productId).count().block());
+    sendCreateRecommendationEvent(productId, recommendationId);
+    assertEquals(1, (long) repository.findAllByProductId(productId).count().block());
 
-    deleteAndVerifyRecommendationsByProductId(productId, OK);
-    assertEquals(0, repository.findAllByProductId(productId).count().block());
+    sendDeleteRecommendationEvent(productId);
+    assertEquals(0, (long) repository.findAllByProductId(productId).count().block());
 
-    deleteAndVerifyRecommendationsByProductId(productId, OK);
+    sendDeleteRecommendationEvent(productId);
   }
 
   @Test
@@ -127,6 +138,7 @@ class RecommendationServiceApplicationTests extends PostgreSQLTestBase {
             .expectBody();
   }
 
+  // Auxiliary test for HTTP requests, not used due to MESSAGE BROKER.
   private WebTestClient.BodyContentSpec postAndVerifyRecommendation(int productId, int recommendationId, HttpStatus expectedStatus) {
     Recommendation recommendation = new Recommendation(productId, recommendationId, "Author " + recommendationId, recommendationId, "Content " + recommendationId, "SA");
     return client.post()
@@ -139,6 +151,7 @@ class RecommendationServiceApplicationTests extends PostgreSQLTestBase {
             .expectBody();
   }
 
+  // Auxiliary test for HTTP requests, not used due to MESSAGE BROKER.
   private WebTestClient.BodyContentSpec deleteAndVerifyRecommendationsByProductId(int productId, HttpStatus expectedStatus) {
     return client.delete()
             .uri("/v1/recommendation?productId=" + productId)
@@ -146,5 +159,16 @@ class RecommendationServiceApplicationTests extends PostgreSQLTestBase {
             .exchange()
             .expectStatus().isEqualTo(expectedStatus)
             .expectBody();
+  }
+
+  private void sendCreateRecommendationEvent(int productId, int recommendationId) {
+    Recommendation recommendation = new Recommendation(productId, recommendationId, "Author " + recommendationId, recommendationId, "Content " + recommendationId, "SA");
+    Event<Integer, Recommendation> event = new Event<>(CREATE, productId, recommendation);
+    messageProcessor.accept(event);
+  }
+
+  private void sendDeleteRecommendationEvent(int productId) {
+    Event<Integer, Recommendation> event = new Event<>(DELETE, productId, null);
+    messageProcessor.accept(event);
   }
 }

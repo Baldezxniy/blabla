ed4c7d17d80dd50abe80a25833c313e85f855f09
Мирослав Косюк <87585769+Baldezxniy@users.noreply.github.com>
2023-11-29 17:53:49 +0200

    changes API: create event class

---
+++ api/src/main/java/com/example/api/event/Event.java
@@ -0,0 +1,51 @@
+package com.example.api.event;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
+
+import java.time.ZonedDateTime;
+
+import static java.time.ZonedDateTime.now;
+
+public class Event<K, V> {
+  public enum Type {
+    CREATE,
+    DELETE
+  }
+
+  private final Type eventType;
+  private final K key;
+  private final V data;
+  private final ZonedDateTime eventCreatedAt;
+
+  public Event() {
+    this.eventType = null;
+    this.key = null;
+    this.data = null;
+    this.eventCreatedAt = null;
+  }
+
+  public Event(Type eventType, K key, V data) {
+    this.eventType = eventType;
+    this.key = key;
+    this.data = data;
+    this.eventCreatedAt = now();
+  }
+
+  public Type getEventType() {
+    return eventType;
+  }
+
+  public K getKey() {
+    return key;
+  }
+
+  public V getData() {
+    return data;
+  }
+
+  @JsonSerialize(using = ZonedDateTimeSerializer.class)
+  public ZonedDateTime getEventCreatedAt() {
+    return eventCreatedAt;
+  }
+}

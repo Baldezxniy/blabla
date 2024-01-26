29a4b03d8151c42187ea3589e960912f1efa801f
Мирослав Косюк <87585769+Baldezxniy@users.noreply.github.com>
2024-01-27 00:22:43 +0200

    #7 Added Eureka client in review service files

--- microservices/review-service/src/test/java/com/example/reviewservice/ReviewServiceApplicationTests.java
+++ microservices/review-service/src/test/java/com/example/reviewservice/ReviewServiceApplicationTests.java
@@ -23,7 +23,7 @@ import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static reactor.core.publisher.Mono.just;
 
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT, properties = {"eureka.client.enabled=false"})
 class ReviewServiceApplicationTests extends PostgreSQLTestBase {
   @Autowired
   private WebTestClient client;

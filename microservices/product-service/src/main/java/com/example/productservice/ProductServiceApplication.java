fcfbee260c8dc14bd509a714a2fb472ded43600f
Мирослав Косюк <87585769+Baldezxniy@users.noreply.github.com>
2024-01-27 00:07:52 +0200

    #7 Added Eureka client in product service files

--- microservices/product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
@@ -4,10 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
